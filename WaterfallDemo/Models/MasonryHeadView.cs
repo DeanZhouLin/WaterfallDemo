0ea689da41c2a9e625083eb6536a104b883d6213
DeanZhouLin <funny_zhoulin@163.com>
2014-02-24 18:20:28 +0800

    12

---
+++ WaterfallDemo/Models/MasonryHeadView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace WaterfallDemo.Models
+{
+    public class MasonryHeadView
+    {
+
+
+        public string ViewCssClasss { get; set; }
+        private readonly dynamic _imageInfo = new ExpandoObject();
+        public dynamic ImageInfo
+        {
+            get { return _imageInfo; }
+        }
+
+        private readonly Dictionary<string, dynamic> _captionHeader = new Dictionary<string, dynamic>();
+
+        public Dictionary<string, dynamic> CaptionHeader
+        {
+            get
+            {
+                return _captionHeader;
+            }
+        }
+
+        private readonly Dictionary<string, dynamic> _captionContent = new Dictionary<string, dynamic>();
+
+        public Dictionary<string, dynamic> CaptionContent
+        {
+            get { return _captionContent; }
+        }
+
+    }
+}
